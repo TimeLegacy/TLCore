9b8218b73ed06117eb97839588ccdd39855434d1
Shaun Agostinho <shaunagostinho@gmail.com>
2019-07-10 13:03:38 -0700

    scoreboard utils for server wide scoreboard type thing.

---
+++ src/main/java/net/timelegacy/tlcore/utils/ScoreboardUtils.java
@@ -0,0 +1,25 @@
+package net.timelegacy.tlcore.utils;
+
+import java.util.HashMap;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.ScoreboardManager;
+
+public class ScoreboardUtils {
+
+  private static HashMap<Player, Scoreboard> scoreboards = new HashMap<>();
+
+  public static Scoreboard getScoreboard(Player player) {
+    if (!scoreboards.containsKey(player)) {
+      ScoreboardManager manager = Bukkit.getScoreboardManager();
+      Scoreboard board = manager.getNewScoreboard();
+      scoreboards.put(player, board);
+
+      return board;
+    } else {
+      return scoreboards.get(player);
+    }
+  }
+
+}
