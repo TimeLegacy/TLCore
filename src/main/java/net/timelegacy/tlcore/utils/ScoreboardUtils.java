1f5cb39fee606dc854b6476cfcb7d93a0c23865f
Shaun Agostinho <shaunagostinho@gmail.com>
2019-07-12 00:58:14 -0700

    custom scoreboards using packets?

--- src/main/java/net/timelegacy/tlcore/utils/ScoreboardUtils.java
+++ src/main/java/net/timelegacy/tlcore/utils/ScoreboardUtils.java
@@ -1,5 +1,8 @@
 package net.timelegacy.tlcore.utils;
 
+import java.util.HashMap;
+import java.util.UUID;
+import net.timelegacy.tlcore.datatype.CustomScoreboard;
 import org.bukkit.Bukkit;
 import org.bukkit.scoreboard.Scoreboard;
 
@@ -11,4 +14,17 @@ public class ScoreboardUtils {
     return scoreboard;
   }
 
+  private static HashMap<UUID, CustomScoreboard> customScoreboards = new HashMap<>();
+
+  public static void saveCustomScoreboard(UUID uuid, CustomScoreboard customScoreboard) {
+    customScoreboards.put(uuid, customScoreboard);
+  }
+
+  public static CustomScoreboard getCustomScoreboard(UUID uuid, CustomScoreboard customScoreboard) {
+    if (customScoreboards.containsKey(uuid)) {
+      return customScoreboards.get(uuid);
+    } else {
+      return null;
+    }
+  }
 }
