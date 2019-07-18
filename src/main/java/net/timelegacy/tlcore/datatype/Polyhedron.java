ca85da2f4099455518ab9fbccbc9676d03d8a560
piajesse <me@piajesse.com>
2019-07-17 18:29:06 -0700

    Renamed Polygon to Polyhedron

--- src/main/java/net/timelegacy/tlcore/datatype/Polyhedron.java
+++ src/main/java/net/timelegacy/tlcore/datatype/Polyhedron.java
@@ -1,19 +1,20 @@
 package net.timelegacy.tlcore.datatype;
 
 import java.util.ArrayList;
+import org.bukkit.util.Vector;
 
-public class Polygon {
+public class Polyhedron {
 
   protected AABB3D edgeShell;
   protected ArrayList<AABB3D> polyParts;
 
-  public Polygon(AABB3D poly) {
+  public Polyhedron(AABB3D poly) {
     edgeShell = poly;
     polyParts = new ArrayList<>();
     polyParts.add(poly);
   }
 
-  public Polygon(AABB3D[] poly) {
+  public Polyhedron(AABB3D[] poly) {
     if (poly.length > 0) {
       edgeShell = poly[0];
       polyParts = new ArrayList<>();
@@ -23,10 +24,16 @@ public class Polygon {
     }
 
   }
+  public Polyhedron(double xCenter, double yCenter, double zCenter, double xSize, double ySize, double zSize){
+    AABB3D poly = new AABB3D(new Vector(xCenter, yCenter, zCenter), new Vector(xSize, ySize, zSize));
+    edgeShell = poly;
+    polyParts = new ArrayList<>();
+    polyParts.add(poly);
+  }
 
-  public static boolean isInside(Polygon polygon, AABB3D object) {
-    if (AABB3D.collides(polygon.edgeShell, object)) {
-      for (AABB3D polyPart : polygon.polyParts) {
+  public static boolean isInside(Polyhedron polyhedron, AABB3D object) {
+    if (AABB3D.collides(polyhedron.edgeShell, object)) {
+      for (AABB3D polyPart : polyhedron.polyParts) {
         if (AABB3D.collides(polyPart, object)) {
           return true;
         }
